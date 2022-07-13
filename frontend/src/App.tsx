0686af87360fe3d40e8e94ca7629c25df50a6f02
Jackson Petry <jackson_p@outlook.com>
2022-07-13 00:32:20 -0300

    Aula ate 1h10min

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import NotificationButton from "./components/notificationButton"
-
+import Header from "./components/Header"
 function App() {
   return (
     <>
-  <h1>ola mundo</h1>
+  <Header />
+<NotificationButton />
 <NotificationButton />
 </>
   )
